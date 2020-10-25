152b79e6488b123affa27672d066074592eaf191
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-10-25 18:18:34 +0530

    commit initial change

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,45 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,//variable needed to be changed
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderChoice.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: { left: "0px" }
     };
+    this.renderChoice = this.renderChoice.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.keyDown = this.keyDown.bind(this);
+  }
 
-    //call back function
-    buttonClickHandler() {
-   
-   }
-    renderChoice() {
-		if(this.state.renderBall){return <div className="ball" style={this.state.ballPosition}></div>}
-		 else   return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-    }
+  buttonClickHandler() {
+    this.setState({ renderBall: !this.state.renderBall });
+  }
 
-    //bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
+  renderChoice() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else
+      return (
+        <button onClick={this.buttonClickHandler}>Click For One Ball</button>
+      );
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderChoice()}
-            </div>
-        )
-    }
+  //bind ArrowRight keydown event
+
+  keyDown(code) {
+    this.setState({ ballPosition: { left: "5px" } });
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", (el) => this.keyDown(el.keyCode));
+  }
+
+  render() {
+    return <div className="playground">{this.renderChoice()}</div>;
+  }
 }
-
 
 export default App;
